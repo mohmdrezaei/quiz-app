88529dcd53f72ce1b8428c355da118b12fb4ff03
mohmdrezaei <mhwmdrz9@gmail.com>
2024-08-11 13:19:33 +0330

    Show Questions

--- js/game.js
+++ js/game.js
@@ -1,19 +1,39 @@
 import formatData from "./helper.js";
-const URL = "https://opentdb.com/api.php?amount=10&difficulty=medium";
+
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
+const questionText = document.getElementById("question-text");
+const answerList = document.querySelectorAll(".answer-text");
+const URL =
+  "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 let formattedData = null;
+let questionIndex = 0;
+let correctAnswer = null;
 
 const fetchData = async () => {
   const res = await fetch(URL);
   const json = await res.json();
- formattedData = formatData(json.results);
- console.log(formattedData)
+  formattedData = formatData(json.results);
   start();
 };
 
 const start = () => {
+  showQuestion();
   loader.style.display = "none";
   container.style.display = "block";
 };
+
+const showQuestion = () => {
+  const { question, answers, correctAnswerIndex } =
+    formattedData[questionIndex];
+  correctAnswer = correctAnswerIndex;
+  questionText.innerText = question;
+  answerList.forEach((button, index) => {
+    button.innerText = answers[index];
+  });
+};
+const checkAnswer = ()=>{}
 window.addEventListener("load", fetchData);
+answerList.forEach((button, index) => {
+  button.addEventListener("click", checkAnswer)
+})
--- js/helper.js
+++ js/helper.js
@@ -1,11 +1,10 @@
 const formatData = (qustionData) => {
-  console.log(qustionData);
   const result = qustionData.map((item) => {
     const questionObject = { question: item.question };
     const answers = [...item.incorrect_answers];
     const correctAnswerIndex = Math.floor(Math.random() * 4);
     answers.splice(correctAnswerIndex, 0, item.correct_answer);
-    questionObject.answes = answers;
+    questionObject.answers = answers;
     questionObject.correctAnswerIndex = correctAnswerIndex;
     return questionObject;
   });
